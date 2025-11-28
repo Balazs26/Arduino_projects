c7bb1010de6da90d19595a2633ca815511ef1569
Balazs26 <131625311+Balazs26@users.noreply.github.com>
2025-11-28 05:58:10 +0100

    Update STibi.ino

--- STibi.ino
+++ STibi.ino
@@ -1,5 +1,7 @@
 #include <Key.h>
 #include <Keypad.h>
+#include <LiquidCrystal.h>
+
 String PASSWORD = "1245";
 String text;
 String Status = "CLOSED";
@@ -7,10 +9,19 @@ String Status = "CLOSED";
 int openTime = 3000;
 int noteTime = 500;
 
-int BUZZER = 10;
+int BUTTON = 44;
+
+int BUZZER = 22;
 
 byte y = 0 ;
 byte z = 0 ;
+
+int LCD_RS = 24;
+int LCD_ENABLE = 26;
+int LCD_D4 = 28;
+int LCD_D5 = 30;
+int LCD_D6 = 32;
+int LCD_D7 = 34;
 
 char k[4][4]={
   {'1','2','3','A'},
@@ -23,18 +34,20 @@ byte cpins[4]={6,7,8,9};
 char key;
   
 Keypad mypad (makeKeymap(k),rpins,cpins,4,4);
+LiquidCrystal lcd(LCD_RS, LCD_ENABLE, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
+//LiquidCrystal lcd(LCD_RS, 12, 11, 6, 5, 4, 3);
 
 void OPEN_DOOR(){
   Serial.println("BELEPES ENGEDELYEZVE!");
   Status = "OPEN";
 
-  tone(BUZZER, 2000, noteTime);
-  
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("BELEPES");
+  lcd.setCursor(0, 1);
+  lcd.print("ENGEDELYEZVE!");
 
-  delay(openTime);
-  Serial.println("AZ AJTO VISSZA LETT ZARVA!");
-  Status = "CLOSED";
-  text = "";
+  tone(BUZZER, 2000, noteTime);
 }
 
 void WRONG_PASSWORD(){
@@ -45,13 +58,31 @@ void WRONG_PASSWORD(){
     tone(BUZZER, 1000, noteTime/3);
     delay(noteTime/2);
   }
+
+  lcd.setCursor(0, 1);
+  lcd.print("HIBAS JELSZO!");
+
+  delay(3000);
+
+  lcd.setCursor(0, 1);
+
+  String str;
+  for (int i = 0; i<16; i++){
+    str = str + " ";
+  }
+
+  lcd.print(str);
 }
 
 void setup() 
    {
-  
+    pinMode(BUTTON, INPUT);
     Serial.begin(9600);
+    lcd.begin(16,2);
     pinMode(BUZZER, OUTPUT);
+
+    lcd.setCursor(0,0);
+    lcd.print("ADD MEG A KODOT:");
   }
 
 void loop() {
@@ -60,6 +91,22 @@ void loop() {
     text = text + key;
     Serial.println("text: " + text);
 
+    lcd.setCursor(0,1);
+
+    bool displayCode = false;
+
+    String str;
+    if(displayCode) {
+      str = text;
+    } else {
+      for (int i = 0; i< text.length(); i++){
+        str = str + "*";
+      }
+    }
+      
+    
+    lcd.print(str);
+
     if(text == PASSWORD){
       OPEN_DOOR();
     } else {
@@ -67,6 +114,15 @@ void loop() {
         WRONG_PASSWORD();
       }
     }
+  } else {
+    bool pushed = false;
+    if(Status == "OPEN" and digitalRead(BUTTON)){
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("FIOK NYITVA!");
+      pushed = true;
+
+    }
+
   }
 }
-
